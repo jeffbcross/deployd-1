256d318ad15d649d02db6b0e53c5372b5c9d1489
Jeff Cross <middlefloor@gmail.com>
2013-12-19 23:43:12 -0800

    feat(server): add middleware support through server & router

---
+++ test/connect-middleware.integration.js
@@ -0,0 +1,34 @@
+var Server = require('../lib/server')
+    sinon = require('sinon');
+
+describe('Connect Middleware', function () {
+  var req, res, next;
+
+  beforeEach(function () {
+    req = {url: 'foo', connection: {encrypted: false}, headers: {accept: '*'}};
+    res = {body: 'bar', on: function () {}, setHeader: function () {}, getHeader: function () {}};
+    next = sinon.spy();
+  });
+
+  it.only('should call next after handling the route', function (done) {
+    var server = new Server();
+    server.isTest = true;
+    server.handleRequest(req, res, next);
+
+    process.nextTick(function () {
+      expect(next.callCount).to.equal(1);
+      done();
+    });
+
+  });
+
+  //Write some real tests to see that appropriate items are being added to req/res
+  it('should work with the collection resource', function () {
+    expect(false).to.be(true);
+  });
+
+
+  it('should work with the users collection', function () {
+
+  });
+});
